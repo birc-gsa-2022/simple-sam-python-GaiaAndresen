0e675d95f5d3077a301f047754305bd648db281d
GaiaAndresen <gaia.ulv@gmail.com>
2022-09-03 22:17:47 +0200

    Passes test

--- src/tosam.py
+++ src/tosam.py
@@ -8,10 +8,10 @@ def main():
         type=argparse.FileType('r')
     )
     args = argparser.parse_args()
-    print(args)
+    #print(args)
     for line in args.mas:
         chrom, read_name, read_str, pos = line.split('\t')
-        # Output as Simple-SAM
+        print(read_name, chrom, int(pos)+1, f'{len(read_str)}M', read_str, sep="\t")
 
 
 if __name__ == '__main__':
